44911890e6179e5e03b786d64f640a779c89f348
Steve McCrumb <steve@pofd.com>
2013-05-24 00:27:52 +0200

    auto generate of calendar

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,128 +4,28 @@
 <div id="palace-grid-holder" class="">
   <?php
     $count = 0;
-    
-    for($e = 0; $e < count($events); $e++):
-    if(($e % $columns == 0)):
-  ?>
-  <div class="grid-row clearfix">
-    <?php endif; ?>
-  <div class="item-holder zoomTarget">
-    <div class="door">
-      <div class="date"><?php echo date('j',$events[$e]['date']);?><sup><?php echo date('S',$events[$e]['date']);?></sup></div>
-    </div>
-  </div>
-  <?php
-  if(($e % $columns == 0)):
-  ?>
-  </div>
-  <?php endif; ?>
-  <?php endfor; ?>
-  <div class="grid-row clearfix">
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
+    $currentRow = 0;
+    $currentColumn = 0;
+    for($r = 0; $r < $rows; $r++):
+    ?>
+      <div class="grid-row clearfix">
+    <?php
+      for($c = 0; $c < $rows; $c++):
+       ?>
+       <div class="item-holder zoomTarget">
+         <div class="door">
+           <div class="date"><?php echo date('j',$events[$count]['date']);?><sup><?php echo date('S',$events[$count]['date']);?></sup></div>
+         </div>
+       </div>
+      <?php
+      $count++;
+      endfor;
+      ?>
       </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-  </div>
+    <?php  
+    endfor;
+    ?>
   
-  <div class="grid-row clearfix">
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door event">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-  </div>
-  
-  <div class="grid-row clearfix">
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-  </div>
-  
-  <div class="grid-row clearfix">
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-    
-    <div class="item-holder zoomTarget">
-      <div class="door">
-        <div class="date">May 29<sup>th</sup></div>
-      </div>
-    </div>
-  </div>
-</div>
-
 </div>
 </div>
 
