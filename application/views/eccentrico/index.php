ee0ff27be913c2ef2ee75219171a9cd1c0c15c31
Francesco Chessa Orrù <francescochessaorru@gmail.com>
2013-05-28 22:45:59 +0200

    Testo eccentrico index

--- application/views/eccentrico/index.php
+++ application/views/eccentrico/index.php
@@ -22,9 +22,15 @@
     <div class="span7 offset1">
       <div class="row-fluid">
         <div class="span10">
-          <p>In the first place, you are struck by the general contrast between these heads. Both are massive enough in all conscience; but there is a certain mathematical symmetry in the Sperm Whale's which the Right Whale's sadly lacks. There is more character in the Sperm Whale's head. As you behold it, you involuntarily yield the immense superiority to him, in point of pervading dignity. In the present instance, too, this dignity is heightened by the pepper and salt colour of his head at the summit, giving token of advanced age and large experience. In short, he is what the fishermen technically call a "grey-headed whale."</p>
+          <p>Eccentrico is an installation of exceptional clothes and unique accessories, a journey through the imagination and
+          a study of the capacity for transformation, playing with shapes and materials to explore cultural connections and
+          artistic symbols. It is this aspect – of the unusual – that I wished to highlight, because it represents an exuberant form
+          of creativity that extends the concept of minimalism and rigour linked with my fashion design. And as simplicity of
+          line does not equal simplicity of thought, I have underscored the wealth and freedom of inspiration that form the
+          foundation of my work and that tell a long and unexpected story.</br>My own story, beyond the clichés.</p>
+          
+          <p style="text-align:right;">Giorgio Armani</span></p>
 
-          <p>Let us now note what is least dissimilar in these heads&mdash;namely, the two most important organs, the eye and the ear. Far back on the side of the head, and low down, near the angle of either whale's jaw, if you narrowly search, you will at last see a lashless eye, which you would fancy to be a young colt's eye; so out of all proportion is it to the magnitude of the head.</p>
           </div>
         </div>
       </div>
