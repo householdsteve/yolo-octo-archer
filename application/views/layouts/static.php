23887d48bf8810cf983fcbd7adf623e8a97d857b
Steven McCrumb <steve@pofd.com>
2013-05-29 21:52:10 +0200

    added tracking to static

--- application/views/layouts/static.php
+++ application/views/layouts/static.php
@@ -30,6 +30,15 @@
   		  <img src="<?php echo base_url();?>assets/img/logo.gif" width="269" height="328" alt="One night only Roma"/>
   		</div>
   	</div>
-
+    <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-2086908-9']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    </script>
   </body>
 </html>
