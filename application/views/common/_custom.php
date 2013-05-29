b156484aa3535cf810041a9f4d5d3306596d2ddf
Steven McCrumb <steve@pofd.com>
2013-05-29 15:48:13 +0200

    fixing shoddy work

--- application/views/common/_custom.php
+++ application/views/common/_custom.php
@@ -1,5 +1,5 @@
 <div class="social content custom">
-  <img src="/assets/img/placeholder.png" width="612" height="612" alt="Tag IT">
+  <img src="<?php echo base_url(); ?>assets/img/placeholder.png" width="612" height="612" alt="Tag IT">
   <div class="above">
     <p><?php echo $content;?></p>
     <h3>#ONENIGHTONLYROMA</h3>
