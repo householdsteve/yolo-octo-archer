19d7d395b75d3ffcef16ee607bf9d7ef4bece83f
Steven McCrumb <steve@pofd.com>
2013-05-30 00:54:18 +0200

    production ready

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,7 +40,7 @@
 $route['tumblrimport'] = 'tumblrimport';
 $route['home/countdown/(:any)'] = 'home/countdown/$1';
 $route['home/events'] = 'home/events/roma';
-$route['default_controller'] = "home/waiting";
+$route['default_controller'] = "home";
 $route['404_override'] = '';
 
 
