a59d4077100694608880a81a83c1c20c78107fcb
Steven McCrumb <steve@pofd.com>
2013-06-03 14:43:43 +0200

    changed tokyo slideshow layout

--- application/views/events/tokyo.php
+++ application/views/events/tokyo.php
@@ -4,8 +4,7 @@
 <div class="row-fluid top-space">
   <div class="span12">
     <div class="row-fluid">
-      <div class="span3 offset1">
-        <h2>SEE THE FACES</h2>
+      <div class="span7">
         <div class="royalSlider rsDefault">
             <?php for($i=1; $i < 12; $i++):?>   
             <a class="rsImg" href="<?php echo base_url()?>assets/img/events/one_night_only-tokyo_<?php echo $i;?>.jpg"> </a>
@@ -13,9 +12,9 @@
         </div>
       </div>
 
-  <div class="span7 offset1">
+  <div class="span5">
     <div class="row-fluid">
-      <div class="span7">
+      <div class="span11">
         <h1>TOKYO 2007</h1>
         <p>“The idea behind One Night Only at Budokan is to create an evening reflecting the same sense of energy and excitement that Ginza now represents. Over the last few years Ginza has become one of the world’s unique fashion retail destinations, quickly joining the likes of New York’s Fifth Avenue, London’s Bond Street, Milan’s Via Montenapoleone and Paris’s Avenue Montaigne as a fashion and luxury shopping destination.” Giorgio Armani said.</p>
         </div>
