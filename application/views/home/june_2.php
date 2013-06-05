47efdce74fb39439d0593ecab8eb39deec55840e
Francesco Chessa Orrù <francescochessaorru@gmail.com>
2013-06-05 09:33:51 +0200

    new Preziosi interview

--- application/views/home/june_2.php
+++ application/views/home/june_2.php
@@ -1,12 +1,12 @@
 <?php if(!$isAjax):?>
 <div class="cd-page">
   <div class="cd-content">
-    <iframe width="100%" height="<?php echo $h;?>" src="http://www.youtube.com/embed/_Q2BoiEHLSA?wmode=transparent&rel=0&showinfo=0&modestbranding&autohide=1" frameborder="0" allowfullscreen></iframe>
+    <iframe width="100%" height="<?php echo $h;?>" src="http://www.youtube.com/embed/mThGM_XnyCk?wmode=transparent&rel=0&showinfo=0&modestbranding&autohide=1" frameborder="0" allowfullscreen></iframe>
   </div>
   <a class="cd-close" href="<?php echo base_url();?>">X</a>
 </div>
 <?php else: ?>
 <div>
-    <iframe width="100%" height="<?php echo $h;?>" src="http://www.youtube.com/embed/_Q2BoiEHLSA?wmode=transparent&rel=0&showinfo=0&modestbranding&autohide=1" frameborder="0" allowfullscreen></iframe>
+    <iframe width="100%" height="<?php echo $h;?>" src="http://www.youtube.com/embed/mThGM_XnyCk?wmode=transparent&rel=0&showinfo=0&modestbranding&autohide=1" frameborder="0" allowfullscreen></iframe>
 </div>
 <?php endif; ?>
