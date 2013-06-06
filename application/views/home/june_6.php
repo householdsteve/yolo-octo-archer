668a9da91969df55de0c32b96332e68d97d1a5cb
Francesco Chessa Orrù <francescochessaorru@gmail.com>
2013-06-06 15:35:19 +0200

    1on3 photo gallery

--- application/views/home/june_6.php
+++ application/views/home/june_6.php
@@ -1,3 +1,12 @@
-<div class="row-fluid">
-  <div class="span12"><h1>SORRY, NO SNEAK PEEKS!</h1></div>
+<?php if(!$isAjax):?>
+<a class="cd-close fixed" href="<?php echo base_url();?>">X</a>
+<?php endif; ?>
+<div class="row-fluid">    
+  <div class="span12 nero">
+    <div class="royalSlider rsEvent">
+        <?php for($i=4; $i < 13; $i++):?>   
+        <a class="rsImg" href="<?php echo base_url()?>assets/img/event-rome/one_night_only-rome_<?php echo $i;?>.jpg"> </a>
+        <?php endfor;?>
+    </div>
+  </div>
 </div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -187,6 +187,34 @@ function activateInternalGalleries(){
                    navigateByCenterClick: true
                }
     });
+    
+    $(".royalSlider.rsEvent").royalSlider({
+         loop:true,
+         imageAlignCenter:true,
+         imageScaleMode: 'fit',
+         autoScaleSlider: true,
+         arrowsNavHideOnTouch: false,
+         arrowsNavAutoHide:true,
+         arrowsNav:true,
+         width:'100%',height:'100%',
+         autoScaleSlider:true,autoScaleSliderWidth:1200,
+         autoScaleSliderHeight:500,slidesSpacing:0,
+         imgHeight:570,
+         autoPlay: {
+             		// autoplay options go gere
+             		enabled: true,
+             		pauseOnHover: true
+         },
+         visibleNearby: {
+                     enabled: true,
+                     centerArea: 0.5,
+                     center: false,
+                     breakpoint: availableWidth/2,
+                     breakpointCenterArea: 0.64,
+                     navigateByCenterClick: true
+                 }
+      });
+    
 }
 
 function callCountdownScripts(e){
