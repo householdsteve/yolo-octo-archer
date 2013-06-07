4e937f98d945efe852a9c8b7b7c8a68feae9bcb1
Steven McCrumb <steve@pofd.com>
2013-06-07 12:47:56 +0200

    swapped out rome video in rome section

--- application/views/events/rome.php
+++ application/views/events/rome.php
@@ -1,7 +1,6 @@
-<script type="text/javascript" charset="utf-8">
-  PageAttr.hideTimer = true;
-</script>
-<div id="live-streaming" class="container">
-  <iframe id="frame1" src="http://live.armani.com/live.aspx?e=armani-one-night-only" width="100%" align="left" scrolling="no" frameborder="0"></iframe>
+<div class="cd-page">
+  <div class="cd-content">
+    <iframe src="http://youtube.com/embed/uDOKLIbOgN4?wmode=transparent&rel=0&showinfo=0&modestbranding&autohide=1;width=100%" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+  </div>
   <a class="cd-close" href="<?php echo base_url();?>home">X</a>
 </div>
