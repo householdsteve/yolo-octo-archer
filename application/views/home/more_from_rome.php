1f4c4592b95b2afb4e7a0bd1c8f53a7f9e13d84e
Francesco Chessa Orrù <francescochessaorru@gmail.com>
2013-07-01 10:57:19 +0200

    added best of

--- application/views/home/more_from_rome.php
+++ application/views/home/more_from_rome.php
@@ -1,9 +1,36 @@
 <div class="row-fluid">
   <div class="span12">
      <div id="back-to-count">
-     <h3 class="title"><a href="home" class="mfr arrow left"><span><</span>BACK TO COUNTDOWN</a></h3>
+     <h3 class="title"><a href="/home" class="mfr arrow left"><span><</span>BACK TO HOME</a></h3>
      </div>
    </div>
+</div>
+
+   
+<div class="row-fluid">   
+     <div class="span12">
+       <iframe src="http://youtube.com/embed/JZqhD1giG7c?rel=0&showinfo=0&modestbranding&autohide=1;width=100%&amp;height=480" width="100%" height="446" frameborder="0" allowfullscreen=""></iframe>
+       <div class="sub-title">
+         <h1>TAKE A LOOK AT THE BEST MOMENTS FROM THE EVENT</h1>
+       </div>
+     </div>
+</div>
+
+<div class="row-fluid nero">    
+  <div class="span12 nero">
+    <div class="royalSlider rsMoreFromRome">
+        <?php for($i=1; $i < 10; $i++):?>   
+        <a class="rsImg" href="<?php echo base_url()?>assets/img/beauty/ShowBeauty-<?php echo $i;?>.jpg"> </a>
+        <?php endfor;?>
+    </div>
+    <div class="sub-title">
+       <h1>BEAUTY LOOKS FROM THE FASHION SHOW</h1>
+     </div>
+  </div>
+</div>
+
+
+<div class="row-fluid">    
    <div class="span12">
      <div class="span6 more-rome">
         <iframe src="http://youtube.com/embed/wtrpWEZFTeU?wmode=transparent&rel=0&showinfo=0&modestbranding&autohide=1;width=100%&amp;height=480" width="100%" height="300" frameborder="0" allowfullscreen></iframe>
