bde00625f8cb554e349f6b1f74771e56a44d70f4
stevegb <steve@pofd.com>
2013-10-24 05:57:00 -0400

    temp remove roma

--- roma/application/views/preview/ondemand.php
+++
@@ -1,4 +0,0 @@
-<div id="live-streaming" class="container">
-  <iframe id="frame1" src="http://live.armani.com/live.aspx?e=test-ondemand" width="100%" align="left" scrolling="no" frameborder="0"></iframe>
-  <a class="cd-close" href="<?php echo base_url();?>home">X</a>
-</div>
